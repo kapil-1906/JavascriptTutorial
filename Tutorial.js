5e59d8d77aedd569aad568daa02d53285655ec43
Kapil Kadam <kapil.k0609@gmail.com>
2023-07-11 03:35:20 +0530

    implicit and explicit type conversion

---
+++ Tutorial.js
@@ -0,0 +1,16 @@
+const lastName = "Sharma";
+const middleName = "";
+
+if(middleName){
+    console.log("having a middle name");
+}else if(lastName){
+    console.log("having a last name");
+}else{
+    console.log("No value");
+}
+
+
+const a = "130";
+const b = parseFloat(a);
+
+console.log(b);
